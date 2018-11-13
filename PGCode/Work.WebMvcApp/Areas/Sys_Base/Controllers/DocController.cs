c437f7260691a1267d90480c95728aa7c621e595
好就 莊 <sdec0817@gmail.com>
2018-11-13 16:38:50 +0800

    +page

---
+++ PGCode/Work.WebMvcApp/Areas/Sys_Base/Controllers/DocController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Data;
+using System.IO;
+
+using ProcCore.NetExtension;
+using ProcCore.ReturnAjaxResult;
+
+namespace DotWeb.Areas.Sys_Base.Controllers
+{
+    public class DocController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
